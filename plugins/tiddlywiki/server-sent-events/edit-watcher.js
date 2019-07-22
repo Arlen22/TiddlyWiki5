8da523ddeaa41ebad5e19b5a7078bf38e1515a7f
arlen22 <arlenbee@gmail.com>
2019-07-22 13:53:13 -0400

    Update edit-watcher.js

--- plugins/tiddlywiki/server-sent-events/edit-watcher.js
+++ plugins/tiddlywiki/server-sent-events/edit-watcher.js
@@ -6,7 +6,7 @@ module-type: startup
 Command processing
  
 \*/
-(function(){
+(function () {
 // console.log("test");
 
 exports.name = "edit-watcher";
@@ -24,30 +24,25 @@ exports.startup = function () {
       $tw.serverEvents.broadcastEvent('server-refresh', JSON.stringify(res));
     });
   } else {
-    $tw.serverEvents.addEventListener(
-      "server-refresh",
-      /**
-       * @param {MessageEvent} msg
-       */
-      function serverRefresh(msg) {
-        // console.log(msg);
-        var changes = JSON.parse(msg.data);
-        var filteredChanges = $tw.syncer.filterFn.call(this.wiki,function(callback) {
-          Object.keys(changes).forEach(function(title) {
-            callback($tw.syncer.wiki.getTiddler(title), title);
-          })
-        });
-        filteredChanges.forEach(function (title) {
-          if(changes[title] === "deleted" && $tw.syncer.tiddlerInfo[title]){
-            //This could delete a newly created tiddler with the same name on the server
-            //since the syncer will try deleting it from the server again.
-            $tw.wiki.deleteTiddler(title);
-            // So we delete the sync task before it can be processed. 
-            delete $tw.syncer.taskQueue[title];
-          }
-        });
-        $tw.syncer.syncFromServer();
-      }
+    $tw.serverEvents.addEventListener("server-refresh",/** @param {MessageEvent} msg */ function serverRefresh(msg) {
+      // console.log(msg);
+      var changes = JSON.parse(msg.data);
+      var filteredChanges = $tw.syncer.filterFn.call(this.wiki, function (callback) {
+        Object.keys(changes).forEach(function (title) {
+          callback($tw.syncer.wiki.getTiddler(title), title);
+        })
+      });
+      filteredChanges.forEach(function (title) {
+        if (changes[title] === "deleted" && $tw.syncer.tiddlerInfo[title]) {
+          //This could delete a newly created tiddler with the same name on the server
+          //since the syncer will try deleting it from the server again.
+          $tw.wiki.deleteTiddler(title);
+          // So we delete the sync task before it can be processed. 
+          delete $tw.syncer.taskQueue[title];
+        }
+      });
+      $tw.syncer.syncFromServer();
+    }
     );
   }
 }
